a1dae6608e1047619cc7e5c8328d153577147ae0
piersdeseilligny <piers.deseilligny+github@outlook.com>
2019-08-06 18:23:43 +0200

    fix for invalid json in storage

--- managers/storage.js
+++ managers/storage.js
@@ -3,7 +3,13 @@ var storage = {};
 storage.load = function(name, structure, resolve){
     sqlcon.query("SELECT * FROM Storage WHERE name=?", [name], function(err,results){
         if(results != undefined && results.length != undefined && results.length>0){
-            var result = JSON.parse(results[0]);
+            if(results[0]!=undefined)
+            var result;
+            try {
+                console.log("Loading " + name + " from storage")
+                console.log("json="+results[0]);
+                result = JSON.parse(results[0]);
+            }catch{}
             if(result != undefined){
                 resolve(result);
             }
