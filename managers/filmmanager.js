84d3cb782a085472b0c92091eb6fe57752f70cde
piersdeseilligny <piers.deseilligny+github@outlook.com>
2019-08-08 17:21:58 +0200

    fix dumb test

--- managers/filmmanager.js
+++ managers/filmmanager.js
@@ -122,17 +122,22 @@ HAVING
     LIMIT 1
   )
 )`
-sql = `SELECT * FROM Films WHERE id in (SELECT id COUNT(*) as how_many_shared_tags
+sql = `
+SET @item = ?;
+SELECT * FROM Films WHERE id in (SELECT id COUNT(*) as how_many_shared_tags
 FROM Films p
 JOIN Taggings pt ON pt.filmid = p.id
-                 AND pt.tagid IN(SELECT tagid FROM Taggings WHERE filmid = 1)
-WHERE p.id != 1
+                 AND pt.tagid IN(SELECT tagid FROM Taggings WHERE filmid = @item)
+WHERE p.id != @item
 GROUP BY p.id
 order by COUNT(*) DESC
 LIMIT 12)`
 
   sqlcon.query(sql, [id], function(err, results){
-      if(err || !results[1]) resolve(undefined);
+      if(err || !results[1]){
+          console.log(err);
+          resolve(undefined);
+      } 
       else resolve(results[1]);
   })
 }
