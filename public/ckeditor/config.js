6b9969314ccc91451de660482ef7427ee1a04938
piersdeseilligny <piers.deseilligny+github@outlook.com>
2019-08-14 14:25:41 +0200

    add fontawesome ckeditor plugin

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -7,4 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraPlugins = 'fontawesome';
+	config.contentsCss = 'https://use.fontawesome.com/releases/v5.8.1/css/all.css';
+	config.allowedContent = true; 
 };
